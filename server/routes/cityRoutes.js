a094ef1dfdd9a7216641364ede4ecec978fbb582
Dianna Paronyan <01234dianna@gmail.com>
2024-06-21 13:19:28 +0400

    Add routes

--- server/routes/cityRoutes.js
+++ server/routes/cityRoutes.js
@@ -6,5 +6,7 @@ const router = Router();
 
 router.post('/', cityValidator, CityController.createCity);
 router.get('/', CityController.getAllCities);
+router.get('/:id', CityController.getCity);
+router.get('/country/:countryId', CityController.getAllCitiesByCountryId);
 
 module.exports = router;
